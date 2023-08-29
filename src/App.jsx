7c68b045b0115704711af0e6d7bde61400710a0f
Joonas Kaartti <joonas.kaartti@gmail.com>
2023-08-29 16:01:24 +0300

    Fixed a minor HTML bug

--- src/App.jsx
+++ src/App.jsx
@@ -83,7 +83,7 @@ const App = () => {
     return (
       <div>
         <h2>blogs</h2>
-        <p>{user.name} logged in {logoutButton()}</p>
+        <div>{user.name} logged in {logoutButton()}</div>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
