ea7ccc855ab3f810cb8bfb377d38251040aeff4c
Joonas Kaartti <joonas.kaartti@gmail.com>
2023-08-29 15:31:46 +0300

    added login functionality (5.1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      console.log('login succesful')
+    } catch (exception) {
+      console.log('wrong credentials')
+    }
+  }
+
   const [blogs, setBlogs] = useState([])
 
   useEffect(() => {
@@ -11,12 +30,48 @@ const App = () => {
     )  
   }, [])
 
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  // if user is logged in, show the blog list
+  if (user) {
+    return (
+      <div>
+        <h2>blogs</h2>
+        <p>{username} logged in</p>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    )
+  }
+
+  // if user is not logged in, show the login form
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <h2>login to application</h2>
+      {!user && loginForm()}
     </div>
   )
 }
