7b496cff189a8bf14985386b2f864606ddb7004c
Joonas Kaartti <joonas.kaartti@gmail.com>
2023-08-30 15:08:24 +0300

    Added the possibility to view more info about a blog (5.7)

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
+import { useState } from "react"
+import blogs from "../services/blogs"
+
+const Blog = ({ blog }) => {
+  let blogStyle = {
+    paddingTop: 7,
+    paddingBottom: 8,
+    paddingLeft: 5,
+    backgroundColor: '#DDF0D0',
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => { setVisible(!visible) }
+  
+  const visibilityStyle = { display: visible ? '' : 'none' }
+  const buttonText = visible ? 'hide' : 'view'
+
+  return (
+  <div style={blogStyle} onClick={toggleVisibility}>
     {blog.title} {blog.author}
-  </div>  
-)
+    <button onClick={toggleVisibility}>{buttonText}</button>
+    <div style={visibilityStyle}>
+      {blog.url}<br/>
+      likes {blog.likes} <button>like</button><br/>
+      {blog.user.name}
+    </div>
+  </div>
+  )
+}
 
 export default Blog
