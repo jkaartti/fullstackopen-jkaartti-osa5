0beadb249ee90029d83ad7f1f6fd1b3160a6a4e8
Joonas Kaartti <joonas.kaartti@gmail.com>
2023-09-20 10:21:14 +0300

    tests for logging in (5.18)

--- cypress/e2e/bloglist.cy.js
+++ cypress/e2e/bloglist.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Test User',
+      username: 'testuser',
+      password: 'testpassword'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:5173')
   })
 
@@ -9,5 +15,24 @@ describe('Blog app', function() {
       .should('contain', 'username')
       .should('contain', 'password')
       .should('contain', 'login')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('testuser')
+      cy.get('#password').type('testpassword')
+      cy.get('#loginButton').click()
+
+      cy.contains('Test User logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('wronguser')
+      cy.get('#password').type('wrongpassword')
+      cy.get('#loginButton').click()
+
+      cy.get('html').should('contain', 'wrong username or password')
+        .and('not.contain', 'logged in')
+    })
   })
 })
